ed22d0d3892deaae99e516afce94af678725f50f
Gabriel Gomes <114439750+Gabrielfgomss@users.noreply.github.com>
2023-02-02 12:54:16 -0300

    Nova linha adicionada por gabriel

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o arquivo local do GitHub")
 console.log("Aplicação conectada ao terminal")
 console.log("Modificação por membro da equipe")
+console.log("Modificação feita por Gabriel")
